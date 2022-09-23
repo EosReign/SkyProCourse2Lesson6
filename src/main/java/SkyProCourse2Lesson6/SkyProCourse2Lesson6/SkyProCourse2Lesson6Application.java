373614179e770ef9c2507600b52ec3be9e281135
EosReign <107928418+EosReign@users.noreply.github.com>
2022-09-23 17:58:15 +0300

    Initial commit

---
+++ src/main/java/SkyProCourse2Lesson6/SkyProCourse2Lesson6/SkyProCourse2Lesson6Application.java
@@ -0,0 +1,13 @@
+package SkyProCourse2Lesson6.SkyProCourse2Lesson6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProCourse2Lesson6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyProCourse2Lesson6Application.class, args);
+	}
+
+}
---
+++ src/test/java/SkyProCourse2Lesson6/SkyProCourse2Lesson6/SkyProCourse2Lesson6ApplicationTests.java
@@ -0,0 +1,13 @@
+package SkyProCourse2Lesson6.SkyProCourse2Lesson6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProCourse2Lesson6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
